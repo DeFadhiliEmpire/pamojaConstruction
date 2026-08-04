4bafb6528bd3ccb67c18322c5a860ac34fc75e82
DeFadhiliEmpire <catarax27@gmail.com>
2026-08-04 16:26:03 +0300

    project fetch

--- projects.js
+++ projects.js
@@ -108,7 +108,7 @@ router.patch("/projects/:id/status", async (req, res) => {
 
 router.get("/projects", async (req, res) => {
   try {
-    const projects = await Projects.find();
+    const projects = await Project.find();
     res.status(200).json({ projects });
   } catch (err) {
     res.status(500).json({ error: "Error fetching projects", err });
