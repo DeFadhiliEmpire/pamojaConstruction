4df5922c86a18a96f2c665e2b6fcc3ade7f9e104
DeFadhiliEmpire <catarax27@gmail.com>
2026-08-18 20:23:31 +0300

    commit secret

--- auth.js
+++ auth.js
@@ -296,7 +296,7 @@ passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIET_SECRET,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     async (accessToken, refreshToken, Profile, done) => {
